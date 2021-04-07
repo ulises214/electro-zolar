36e4f0caa2d70b5857d8b631b5c84603ccfe3a8f
Ulises Ramirez <ulisesraf.ramirez@gmail.com>
2021-04-06 20:05:42 -0500

    Footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,26 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import './App.scss';
 import AppBar from './components/AppBar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <Router>
       <AppBar />
-      <Switch>
-        <Route exact path='/'>
-          pito1
-        </Route>
-        <Route exact path='/instalaciones'>
-          pito2
-        </Route>
-        <Route exact path='/servicio'>
-          pito3
-        </Route>
-      </Switch>
+      <article>
+        <Switch>
+          <Route exact path='/'>
+            pito1
+          </Route>
+          <Route exact path='/instalaciones'>
+            pito2
+          </Route>
+          <Route exact path='/servicio'>
+            pito3
+          </Route>
+        </Switch>
+      </article>
+      <Footer />
     </Router>
   );
 }
