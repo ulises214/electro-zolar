9708c25947ca99a216b00dc79ed9faa43c8e48d7
Ulises Ramirez <ulisesraf.ramirez@gmail.com>
2021-04-09 11:16:23 -0500

    Move routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,28 @@ import './App.scss';
 import AppBar from './components/AppBar';
 import Footer from './components/Footer';
 
+const Routes = () => {
+  return (
+    <Switch>
+      <Route exact path='/'>
+        pito1
+      </Route>
+      <Route exact path='/instalaciones'>
+        pito2
+      </Route>
+      <Route exact path='/servicio'>
+        pito3
+      </Route>
+    </Switch>
+  );
+};
+
 function App() {
   return (
     <Router>
       <AppBar />
       <article>
-        <Switch>
-          <Route exact path='/'>
-            pito1
-          </Route>
-          <Route exact path='/instalaciones'>
-            pito2
-          </Route>
-          <Route exact path='/servicio'>
-            pito3
-          </Route>
-        </Switch>
+        <Routes />
       </article>
       <Footer />
     </Router>
