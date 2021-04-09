b798f90e3d0747e92fc8968e4b1fa408f8fb46a8
Ulises Ramirez <ulisesraf.ramirez@gmail.com>
2021-04-09 11:13:58 -0500

    Footer responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,6 +3,7 @@ import MobileIcon from '../assets/img/icons/mobile.svg';
 import PhoneIcon from '../assets/img/icons/phone.svg';
 import FacebookIcon from '../assets/img/icons/facebook.svg';
 import Logo from './Logo';
+import useMatchScreenSize from '../hooks/useMatchScreenSize';
 const FooterNetworkItem = ({
   src,
   title,
@@ -20,36 +21,38 @@ const FooterNetworkItem = ({
       rel='noreferrer'
     >
       <img className='footer__link__img' src={src} alt='Network link' />
-      <p className='footer__link__p'>{title}</p>
+      <span className='footer__link__span'>{title}</span>
     </a>
   );
 };
 const Footer = () => {
   return (
     <footer className='section section__white'>
-      <div className='networks'>
+      <div className='footer__container'>
         <Logo />
-        <h3>Contáctanos</h3>
-        <FooterNetworkItem
-          src={MailIcon}
-          title='electrozolar@hotmail.com'
-          href='mailto:electrozolar@hotmail.com'
-        />
-        <FooterNetworkItem
-          src={MobileIcon}
-          title='312 157 2677'
-          href='tel:3121572677'
-        />
-        <FooterNetworkItem
-          src={PhoneIcon}
-          title='312 311 9644'
-          href='tel:3123119644'
-        />
-        <FooterNetworkItem
-          src={FacebookIcon}
-          title='@ElectroZolar'
-          href='https://www.facebook.com/ElectroZolar'
-        />
+        <div className='footer__container__links'>
+          <h3>Contáctanos</h3>
+          <FooterNetworkItem
+            src={MailIcon}
+            title='electrozolar@hotmail.com'
+            href='mailto:electrozolar@hotmail.com'
+          />
+          <FooterNetworkItem
+            src={MobileIcon}
+            title='312 157 2677'
+            href='tel:3121572677'
+          />
+          <FooterNetworkItem
+            src={PhoneIcon}
+            title='312 311 9644'
+            href='tel:3123119644'
+          />
+          <FooterNetworkItem
+            src={FacebookIcon}
+            title='@ElectroZolar'
+            href='https://www.facebook.com/ElectroZolar'
+          />
+        </div>
       </div>
     </footer>
   );
