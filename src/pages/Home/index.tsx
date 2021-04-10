14b9448fc8c13e31a82f34d380f9b607a3bb6a73
Ulises Ramirez <ulisesraf.ramirez@gmail.com>
2021-04-10 10:02:33 -0500

    WhoWeAre section

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,13 @@
 import Benefits from './Benefits';
 import Presentation from './Presentation';
+import WhoWeAre from './WhoWeAre';
 
 const HomePage = () => {
   return (
     <>
       <Presentation />
       <Benefits />
+      <WhoWeAre />
     </>
   );
 };
