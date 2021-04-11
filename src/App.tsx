ff8ae2e6f53ad1f124a32873fe7750089ce05c5e
Ulises Rafael Ramírez Martínez <76404082+ulises214@users.noreply.github.com>
2021-04-11 15:06:36 -0500

    Refractor/styles (#4)
    * Fix presentation css
    
    * Fix section's items alignament
    
    * Fix readyToStart css
    
    * Normalize font sizes
    
    * Mixins for aligments
    
    * Disable text decoration for links
    
    * Remove unused css rules

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import AppBar from './components/AppBar';
 import Footer from './components/Footer';
 import HomePage from './pages/Home';
-
 const Routes = () => {
   return (
     <Switch>
