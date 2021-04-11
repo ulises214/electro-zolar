c34b13c4674838ff53a0ebbfdc97a555e6c78316
Ulises Ramirez <ulisesraf.ramirez@gmail.com>
2021-04-11 10:37:05 -0500

    Fix presentation css

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -5,8 +5,10 @@ interface SectionProps {
 }
 export const Section = ({ color, children, className }: SectionProps) => {
   return (
-    <section className={`${className} section section__${color}`}>
-      {children}
+    <section className={`section section__${color}`}>
+      <div className={`section__content-container ${className}`}>
+        {children}
+      </div>
     </section>
   );
 };
